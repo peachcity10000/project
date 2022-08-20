2ed6ef312b28a6fff39c1ce0764741984eddf970
陳冠宇 <apple1051629@outlook.com>
2022-08-20 21:14:32 +0800

    verify

---
+++ static/js/index.js
@@ -0,0 +1,24 @@
+function bbing() {
+    const svgImage = document.getElementById("svg5");
+    var zoom = parseInt(svgImage.style.zoom, 10) || 100;
+    console.log(zoom);
+    zoom += event.wheelDelta / 12;
+    if (1600 * zoom / 100 <= window.innerWidth * 0.70)
+        zoom -= event.wheelDelta / 12;
+
+    if (zoom > 0) svgImage.style.zoom = zoom + "%";
+    return false;
+}
+
+function zoomingByButton(num) {
+    const svgImage = document.getElementById("svg5");
+    const svgContainer = document.getElementsByClassName("mapp");
+    var zoom = parseInt(svgImage.style.zoom, 10) || 100;
+    console.log(zoom);
+    zoom += num
+    if (1600 * zoom / 100 <= window.innerWidth * 0.70)
+        zoom -= num
+
+    if (zoom > 0) svgImage.style.zoom = zoom + "%";
+    return false;
+}
